df0b10ef667e6671e29a109e9a47e2f7316f6202
Ziyanda Babela <ziyandambulawa@gmail.com>
2026-07-20 16:13:26 +0200

    stamp1g_by_source.sql

--- sql/stamp1g_by_source.sql
+++ sql/stamp1g_by_source.sql
@@ -1,4 +1,5 @@
 Which sources attracted the most Stamp 1G hires?
+  
 SELECT
 SOURCE,
 COUNT(*) AS STAMP1G_HIRES
@@ -6,3 +7,8 @@ FROM RECRUITMENT_SOURCE_EFFECTIVENESS
 WHERE WORK_ELIGIBILITY = 'Stamp 1G'
 GROUP BY SOURCE
 ORDER BY STAMP1G_HIRES DESC;
+
+
+Stamp 1G Candidates mostely use the Career site for applications.
+This is useful for workforce planning because Stamp 1G candidates often form part of the international graduate talent pool. 
+Understanding which sources attract this group can help recruitment teams plan targeted sourcing campaigns.
